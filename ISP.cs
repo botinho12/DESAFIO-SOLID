969ae319c697e001b911ba09ab3d00d72f9e3578
botinho12 <botivinicios77@gmail.com>
2024-08-16 00:05:25 -0300

    desafios solid

---
+++ ISP.cs
@@ -0,0 +1,17 @@
+﻿using SOLID.ISP;
+
+namespace SOLID.ISP
+{
+    public class CadastroProduto : ICadastroProduto
+    {
+        public void ValidarDados()
+        {
+            // Validar valor
+        }
+
+        public void SalvarBanco()
+        {
+            // Insert tabela Produto
+        }
+    }
+}
---
+++ LSP.cs
@@ -0,0 +1,13 @@
+﻿using SOLID.ISP;
+
+namespace SOLID.LSP
+{
+    public class Retangulo : Paralelogramo
+    {
+        public Retangulo(int altura, int largura)
+            : base(altura, largura)
+        {
+
+        }
+    }
+}
